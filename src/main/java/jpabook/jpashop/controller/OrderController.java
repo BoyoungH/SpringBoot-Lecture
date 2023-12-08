bd616fd16d9981c8a39c2726b2150c8e6cdf3fc2
BoyoungH <hyong0903@naver.com>
2023-12-08 16:55:02 +0900

    feat: 주문 목록 검색, 취소 추가

--- src/main/java/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -11,10 +11,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -45,11 +42,16 @@ public class OrderController {
         return "redirect:/orders";
     }
 
-//    @GetMapping("/orders")
-//    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model){
-//        List<Order> orders = orderService.findOrders(orderSearch);
-//        model.addAttribute("orders", orders);
-//        return "order/orderList";
-//    }
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model){
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+        return "order/orderList";
+    }
 
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId){
+        orderService.cancelOrder(orderId);
+        return "redirect:/orders";
+    }
 }
